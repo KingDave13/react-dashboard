3e86651345a813b042c30f4816af2eb9605e7227
David Okaliwe <okaliwedavid@gmail.com>
2023-07-20 22:46:20 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+import Team from './scenes/team';
+import Invoices from './scenes/invoices';
+import Contacts from './scenes/contacts';
+import Bar from './scenes/bar';
+import Form from './scenes/form';
+import Line from './scenes/line';
+import Pie from './scenes/pie';
+import FAQ from './scenes/faq';
+import Geography from './scenes/geography';
+
 
 const App = () => {
   const [theme, colorMode] = useMode();
-
 
   return (
     <ColorModeContext.Provider value={colorMode}>
@@ -13,6 +25,9 @@ const App = () => {
           <div className='app'>
             <main className='content'>
               <Topbar />
+              <Routes>
+                <Route path='/' element={<Dashboard />} />
+              </Routes>
             </main>
           </div>
         </ThemeProvider>

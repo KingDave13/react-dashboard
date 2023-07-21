fe439dae2db39e6a4e7f9ebfdef8b8c71df4a7a3
David Okaliwe <okaliwedavid@gmail.com>
2023-07-21 19:20:51 +0100

    Update

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
 import { Box, IconButton, Typography, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { tokens } from '../../theme';
@@ -19,9 +18,14 @@ import  MapOutlinedIcon  from '@mui/icons-material/MapOutlined';
 
 
 const Sidebar = () => {
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+	const [isCollapsed, setIsCollapsed] = useState(false);
+	const [selected, setSelected] = useState('Dashboard');
+
 	return (
 		<div>
-			Sidebar
+			
 		</div>
 	)
 }

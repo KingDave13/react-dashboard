2bcc5c719c0c132ca5b55675fd021c18a3a5a4c1
David Okaliwe <okaliwedavid@gmail.com>
2023-07-22 21:01:08 +0100

    Update

--- src/scenes/team/index.js
+++ src/scenes/team/index.js
@@ -11,7 +11,44 @@ const Team = () => {
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
 
-	return (
+	const columns = [
+		
+	]
 
+	return (
+		<Box m='20px'>
+			<Header title='TEAM' subtitle='Managing Team Members' />
+		     	<Box
+		        m='40px 0 0 0'
+		        height='75vh'
+		        sx={{
+		          '& .MuiDataGrid-root': {
+		            border: 'none'
+		          },
+		          '& .MuiDataGrid-cell': {
+		            borderBottom: 'none'
+		          },
+		          '& .name-column--cell': {
+		            color: colors.greenAccent[300]
+		          },
+		          '& .MuiDataGrid-columnHeaders': {
+		            backgroundColor: colors.blueAccent[700],
+		            borderBottom: 'none'
+		          },
+		          '& .MuiDataGrid-virtualScroller': {
+		            backgroundColor: colors.primary[400]
+		          },
+		          '& .MuiDataGrid-footerContainer': {
+		            borderTop: 'none',
+		            backgroundColor: colors.blueAccent[700]
+		          },
+		          '& .MuiCheckbox-root': {
+		            color: `${colors.greenAccent[200]} !important`
+		          }
+		        }}
+		      >
+		        <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
+		    	</Box>
+		</Box>
 	)
 };

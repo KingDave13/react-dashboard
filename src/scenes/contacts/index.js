d5e722fe9977d4a7fd3dbd33f79c06b42d1a6764
David Okaliwe <okaliwedavid@gmail.com>
2023-07-23 19:52:21 +0100

    Update

--- src/scenes/contacts/index.js
+++ src/scenes/contacts/index.js
@@ -60,41 +60,41 @@ const Contacts = () => {
   ];
 
   return (
-    <Box m="20px">
+    <Box m='20px'>
       <Header
-        title="CONTACTS"
-        subtitle="List of Contacts for Future Reference"
+        title='CONTACTS'
+        subtitle='List of Contacts for Future Reference'
       />
       <Box
-        m="40px 0 0 0"
-        height="75vh"
+        m='40px 0 0 0'
+        height='75vh'
         sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
+          '& .MuiDataGrid-root': {
+            border: 'none'
           },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
+          '& .MuiDataGrid-cell': {
+            borderBottom: 'none'
           },
-          "& .name-column--cell": {
-            color: colors.greenAccent[300],
+          '& .name-column--cell': {
+            color: colors.greenAccent[300]
           },
-          "& .MuiDataGrid-columnHeaders": {
+          '& .MuiDataGrid-columnHeaders': {
             backgroundColor: colors.blueAccent[700],
-            borderBottom: "none",
+            borderBottom: 'none'
           },
-          "& .MuiDataGrid-virtualScroller": {
-            backgroundColor: colors.primary[400],
+          '& .MuiDataGrid-virtualScroller': {
+            backgroundColor: colors.primary[400]
           },
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-            backgroundColor: colors.blueAccent[700],
+          '& .MuiDataGrid-footerContainer': {
+            borderTop: 'none',
+            backgroundColor: colors.blueAccent[700]
           },
-          "& .MuiCheckbox-root": {
-            color: `${colors.greenAccent[200]} !important`,
+          '& .MuiCheckbox-root': {
+            color: `${colors.greenAccent[200]} !important`
           },
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-            color: `${colors.grey[100]} !important`,
-          },
+          '& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+            color: `${colors.grey[100]} !important`
+          }
         }}
       >
         <DataGrid

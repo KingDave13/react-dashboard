2ac99a19490b3ece7d1a76187dbf47135556783f
David Okaliwe <okaliwedavid@gmail.com>
2023-07-24 19:06:24 +0100

    Update

--- src/scenes/calendar/index.js
+++ src/scenes/calendar/index.js
@@ -10,7 +10,7 @@ import {
   ListItem,
   ListItemText,
   Typography,
-  useTheme
+  useTheme,
 } from '@mui/material';
 import Header from '../../components/Header';
 import { tokens } from '../../theme';
@@ -20,7 +20,47 @@ const CalendarComponent = () => {
   const colors = tokens(theme.palette.mode);
   const [currentEvents, setCurrentEvents] = useState([]);
 
-  
+  useEffect(() => {
+
+    // Initialize FullCalendar when the component mounts
+    const calendar = new Calendar(document.getElementById('calendar'), {
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin],
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth',
+      },
+      initialView: 'dayGridMonth',
+      editable: true,
+      selectable: true,
+      selectMirror: true,
+      dayMaxEvents: true,
+      events: [
+        {
+          id: '12315',
+          title: 'All-day event',
+          date: '2022-09-14',
+        },
+        {
+          id: '5123',
+          title: 'Timed event',
+          date: '2022-09-28',
+        },
+      ],
+      select: handleDateClick,
+      eventClick: handleEventClick,
+      eventsSet: (events) => setCurrentEvents(events),
+    });
+
+    // Render the calendar
+    calendar.render();
+
+    // Clean up FullCalendar instance when the component unmounts
+    return () => {
+      calendar.destroy();
+    };
+  }, []); // Empty dependency array to ensure the effect runs only once
+
 
   const handleDateClick = (selected) => {
     const title = prompt('Please enter a new title for your event');
@@ -90,44 +130,11 @@ const CalendarComponent = () => {
 
         {/* CALENDAR */}
         <Box flex="1 1 100%" ml="15px">
-          <FullCalendar
-            height="75vh"
-            plugins={[
-              dayGridPlugin,
-              timeGridPlugin,
-              interactionPlugin,
-              listPlugin,
-            ]}
-            headerToolbar={{
-              left: "prev,next today",
-              center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
-            }}
-            initialView="dayGridMonth"
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            select={handleDateClick}
-            eventClick={handleEventClick}
-            eventsSet={(events) => setCurrentEvents(events)}
-            initialEvents={[
-              {
-                id: "12315",
-                title: "All-day event",
-                date: "2022-09-14",
-              },
-              {
-                id: "5123",
-                title: "Timed event",
-                date: "2022-09-28",
-              },
-            ]}
-          />
+          <div id="calendar" style={{ height: '75vh' }}></div>
         </Box>
       </Box>
     </Box>
   );
 };
 
-export default Calendar;
+export default CalendarComponent;

fbd4f8ad0fa79726a8b0781f4de8108e2532832c
David Okaliwe <okaliwedavid@gmail.com>
2023-07-24 15:02:03 +0100

    Update

--- src/scenes/form/index.js
+++ src/scenes/form/index.js
@@ -39,43 +39,43 @@ const Form = () => {
 		            >
 	            		<TextField
 			                fullWidth
-			                variant="filled"
-			                type="text"
-			                label="First Name"
+			                variant='filled'
+			                type='text'
+			                label='First Name'
 			                onBlur={handleBlur}
 			                onChange={handleChange}
 			                value={values.firstName}
-			                name="firstName"
+			                name='firstName'
 			                error={!!touched.firstName && !!errors.firstName}
 			                helperText={touched.firstName && errors.firstName}
-			                sx={{ gridColumn: "span 2" }}
+			                sx={{ gridColumn: 'span 2' }}
 	              		/>
-	              <TextField
-	                fullWidth
-	                variant="filled"
-	                type="text"
-	                label="Last Name"
-	                onBlur={handleBlur}
-	                onChange={handleChange}
-	                value={values.lastName}
-	                name="lastName"
-	                error={!!touched.lastName && !!errors.lastName}
-	                helperText={touched.lastName && errors.lastName}
-	                sx={{ gridColumn: "span 2" }}
-	              />
-	              <TextField
-	                fullWidth
-	                variant="filled"
-	                type="text"
-	                label="Email"
-	                onBlur={handleBlur}
-	                onChange={handleChange}
-	                value={values.email}
-	                name="email"
-	                error={!!touched.email && !!errors.email}
-	                helperText={touched.email && errors.email}
-	                sx={{ gridColumn: "span 4" }}
-	              />
+			            <TextField
+			                fullWidth
+			                variant='filled'
+			                type='text'
+			                label='Last Name'
+			                onBlur={handleBlur}
+			                onChange={handleChange}
+			                value={values.lastName}
+			                name='lastName'
+			                error={!!touched.lastName && !!errors.lastName}
+			                helperText={touched.lastName && errors.lastName}
+			                sx={{ gridColumn: 'span 2' }}
+			         	/>
+	              		<TextField
+			                fullWidth
+			                variant='filled'
+			                type='text'
+			                label='Email'
+			                onBlur={handleBlur}
+			                onChange={handleChange}
+			                value={values.email}
+			                name='email'
+			                error={!!touched.email && !!errors.email}
+			                helperText={touched.email && errors.email}
+			                sx={{ gridColumn: 'span 4' }}
+	              		/>
 	              <TextField
 	                fullWidth
 	                variant="filled"

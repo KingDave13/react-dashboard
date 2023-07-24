1431d616c20ab3ea327d03bbd053ab31e832446d
David Okaliwe <okaliwedavid@gmail.com>
2023-07-24 15:19:41 +0100

    Update

--- src/scenes/form/index.js
+++ src/scenes/form/index.js
@@ -125,32 +125,32 @@ const Form = () => {
 	          	</form>
 	       	)}
 	      	</Formik>
-    </Box>
-  );
+    	</Box>
+  	);
 };
 
 const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+	/^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
 
 const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required('required'),
-  lastName: yup.string().required('required'),
-  email: yup.string().email('invalid email').required('required'),
-  contact: yup
-    .string()
-    .matches(phoneRegExp, 'Phone number is not valid')
-    .required('required'),
-  address1: yup.string().required('required'),
-  address2: yup.string().required('required')
+	firstName: yup.string().required('required'),
+	lastName: yup.string().required('required'),
+	email: yup.string().email('invalid email').required('required'),
+  	contact: yup
+	    .string()
+	    .matches(phoneRegExp, 'Phone number is not valid')
+	    .required('required'),
+  	address1: yup.string().required('required'),
+  	address2: yup.string().required('required')
 });
 
 const initialValues = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  contact: '',
-  address1: '',
-  address2: ''
+  	firstName: '',
+  	lastName: '',
+  	email: '',
+  	contact: '',
+  	address1: '',
+  	address2: ''
 };
 
 export default Form;

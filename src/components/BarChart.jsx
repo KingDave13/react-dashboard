a571480c7d72df91fbbf5a13f933dfc11abfa4a1
David Okaliwe <okaliwedavid@gmail.com>
2023-07-25 22:17:12 +0100

    Update

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,7 +1,7 @@
 import { useTheme } from '@mui/material';
-import { ResponsiveBar } from "@nivo/bar";
-import { tokens } from "../theme";
-import { mockBarData as data } from "../data/mockData";
+import { ResponsiveBar } from '@nivo/bar';
+import { tokens } from '../theme';
+import { mockBarData as data } from '../data/mockData';
 
 const BarChart = ({ isDashboard = false }) => {
   const theme = useTheme();
@@ -39,13 +39,13 @@ const BarChart = ({ isDashboard = false }) => {
           },
         },
       }}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-      indexBy="country"
+      keys={['hot dog', 'burger', 'sandwich', 'kebab', 'fries', 'donut']}
+      indexBy='country'
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
-      valueScale={{ type: "linear" }}
-      indexScale={{ type: "band", round: true }}
-      colors={{ scheme: "nivo" }}
+      valueScale={{ type: 'linear' }}
+      indexScale={{ type: 'band', round: true }}
+      colors={{ scheme: 'nivo' }}
       defs={[
         {
           id: "dots",

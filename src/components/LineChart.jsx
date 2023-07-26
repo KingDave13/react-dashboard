5033784376558120ca373bb1ec4abfccd0b4cd4f
David Okaliwe <okaliwedavid@gmail.com>
2023-07-26 23:19:42 +0100

    Update

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -47,9 +47,9 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
       margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
       xScale={{ type: 'point' }}
       yScale={{
-        type: "linear",
-        min: "auto",
-        max: "auto",
+        type: 'linear',
+        min: 'auto',
+        max: 'auto',
         stacked: true,
         reverse: false,
       }}

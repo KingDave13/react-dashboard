2b201ecc83c7704a8f7fa7b81d8cdbdac6308a94
David Okaliwe <okaliwedavid@gmail.com>
2023-07-26 23:19:25 +0100

    Update

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -43,9 +43,9 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
           },
         },
       }}
-      colors={isDashboard ? { datum: "color" } : { scheme: "nivo" }} // added
+      colors={isDashboard ? { datum: 'color' } : { scheme: 'nivo' }} // added
       margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: "point" }}
+      xScale={{ type: 'point' }}
       yScale={{
         type: "linear",
         min: "auto",

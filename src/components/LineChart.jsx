82d951828dbe7088c0d036fa0fa43124af9ba8e5
David Okaliwe <okaliwedavid@gmail.com>
2023-07-26 23:21:26 +0100

    Update

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -86,18 +86,18 @@ const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
       useMesh={true}
       legends={[
         {
-          anchor: "bottom-right",
-          direction: "column",
+          anchor: 'bottom-right',
+          direction: 'column',
           justify: false,
           translateX: 100,
           translateY: 0,
           itemsSpacing: 0,
-          itemDirection: "left-to-right",
+          itemDirection: 'left-to-right',
           itemWidth: 80,
           itemHeight: 20,
           itemOpacity: 0.75,
           symbolSize: 12,
-          symbolShape: "circle",
+          symbolShape: 'circle',
           symbolBorderColor: "rgba(0, 0, 0, .5)",
           effects: [
             {

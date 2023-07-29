1639af2cf6551108338d28bfb56191d479271b81
David Okaliwe <okaliwedavid@gmail.com>
2023-07-29 21:52:22 +0100

    Update

--- src/scenes/dashboard/index.js
+++ src/scenes/dashboard/index.js
@@ -198,6 +198,7 @@ const Dashboard = () => {
 		            	Recent Transactions
 		            </Typography>
 	          	</Box>
+
           		{mockTransactions.map((transaction, i) => (
 		            <Box
 		            key={`${transaction.txId}-${i}`}
@@ -233,33 +234,37 @@ const Dashboard = () => {
           		))}
 	       	</Box>
 
-        {/* ROW 3 */}
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          p="30px"
-        >
-          <Typography variant="h5" fontWeight="600">
-            Campaign
-          </Typography>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            mt="25px"
-          >
-            <ProgressCircle size="125" />
-            <Typography
-              variant="h5"
-              color={colors.greenAccent[500]}
-              sx={{ mt: "15px" }}
-            >
-              $48,352 revenue generated
-            </Typography>
-            <Typography>Includes extra misc expenditures and costs</Typography>
-          </Box>
-        </Box>
+        	{/* ROW 3 */}
+        	<Box
+          	gridColumn='span 4'
+          	gridRow='span 2'
+          	backgroundColor={colors.primary[400]}
+          	p='30px'
+        	>
+	            <Typography variant='h5' fontWeight='600'>
+	            	Campaign
+	            </Typography>
+
+		        <Box
+		        display='flex'
+		        flexDirection='column'
+		        alignItems='center'
+		        mt='25px'
+		        >
+		            <ProgressCircle size='125' />
+		            <Typography
+		            variant='h5'
+		            color={colors.greenAccent[500]}
+		            sx={{ mt: '15px' }}
+		            >
+		            	$48,352 revenue generated
+		            </Typography>
+
+		            <Typography>
+		            	Includes extra misc expenditures and costs
+		            </Typography>
+		        </Box>
+        	</Box>
         <Box
           gridColumn="span 4"
           gridRow="span 2"

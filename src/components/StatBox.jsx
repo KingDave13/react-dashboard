ee3c9a1c2184cd7eac6bf349e1366e9efc401cfb
David Okaliwe <okaliwedavid@gmail.com>
2023-07-29 21:07:46 +0100

    Update

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -1,19 +1,19 @@
-import { Box, Typography, useTheme } from "@mui/material";
-import { tokens } from "../theme";
-import ProgressCircle from "./ProgressCircle";
+import { Box, Typography, useTheme } from '@mui/material';
+import { tokens } from '../theme';
+import ProgressCircle from './ProgressCircle';
 
 const StatBox = ({ title, subtitle, icon, progress, increase }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box width="100%" m="0 30px">
-      <Box display="flex" justifyContent="space-between">
+    <Box width='100%' m='0 30px'>
+      <Box display='flex' justifyContent='space-between'>
         <Box>
           {icon}
           <Typography
-            variant="h4"
-            fontWeight="bold"
+            variant='h4'
+            fontWeight='bold'
             sx={{ color: colors.grey[100] }}
           >
             {title}

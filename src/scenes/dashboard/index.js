30d405b9a570a789a41fc555f9926a81d4b8ef58
David Okaliwe <okaliwedavid@gmail.com>
2023-07-29 21:49:05 +0100

    Update

--- src/scenes/dashboard/index.js
+++ src/scenes/dashboard/index.js
@@ -198,38 +198,40 @@ const Dashboard = () => {
 		            	Recent Transactions
 		            </Typography>
 	          	</Box>
-	          		{mockTransactions.map((transaction, i) => (
-			            <Box
-			            key={`${transaction.txId}-${i}`}
-			            display='flex'
-			            justifyContent='space-between'
-			            alignItems='center'
-			            borderBottom={`4px solid ${colors.primary[500]}`}
-			            p='15px'
-			            >
-			              <Box>
+          		{mockTransactions.map((transaction, i) => (
+		            <Box
+		            key={`${transaction.txId}-${i}`}
+		            display='flex'
+		            justifyContent='space-between'
+		            alignItems='center'
+		            borderBottom={`4px solid ${colors.primary[500]}`}
+		            p='15px'
+		            >
+		              	<Box>
 			                <Typography
-			                  color={colors.greenAccent[500]}
-			                  variant="h5"
-			                  fontWeight="600"
+			                color={colors.greenAccent[500]}
+			                variant='h5'
+			                fontWeight='600'
 			                >
-			                  {transaction.txId}
+			                  	{transaction.txId}
 			                </Typography>
-			                <Typography color={colors.grey[100]}>
-			                  {transaction.user}
-			                </Typography>
-			              </Box>
-			              <Box color={colors.grey[100]}>{transaction.date}</Box>
-			              <Box
-			                backgroundColor={colors.greenAccent[500]}
-			                p="5px 10px"
-			                borderRadius="4px"
-			              >
-			                ${transaction.cost}
-			              </Box>
-			            </Box>
-	          ))}
-	        </Box>
+
+		                	<Typography color={colors.grey[100]}>
+		                  		{transaction.user}
+		                	</Typography>
+		              	</Box>
+
+		              	<Box color={colors.grey[100]}>{transaction.date}</Box>
+		              	<Box
+		                backgroundColor={colors.greenAccent[500]}
+		                p='5px 10px'
+		                borderRadius='4px'
+		              	>
+		                	${transaction.cost}
+		              	</Box>
+		            </Box>
+          		))}
+	       	</Box>
 
         {/* ROW 3 */}
         <Box
